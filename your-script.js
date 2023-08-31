00955339ded9fb0412bc6a52765864941be674fa
Distance <50225691+villainize@users.noreply.github.com>
2023-08-30 20:52:01 -0400

    Add files via upload

---
+++ your-script.js
@@ -0,0 +1,11 @@
+// Optional jQuery class toggle for added robustness. 
+
+/* 
+var labelID;
+
+$('label').click(function() {
+  labelID = $(this).attr('for');
+  $('input').removeClass('active');
+  $('#' + labelID).addClass('active');
+}); 
+*/
